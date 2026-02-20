050da941a59609441db251b66c1a1e27c6f1a249
sakshichib <157032642+sakshichib@users.noreply.github.com>
2026-02-20 09:56:26 +0530

    Exp-2 Done!

--- Experiment-2/Sum_having_pairs.cpp
+++ Experiment-2/Sum_having_pairs.cpp
@@ -1,1 +1,21 @@
+int solve(vector<int>& A) {
 
+    const int MOD = 1000000007;
+    long long ans = 0;
+    int n = A.size();
+
+    for(int bit = 0; bit < 32; bit++) {
+
+        long long ones = 0;
+
+        for(int x : A)
+            if(x & (1LL << bit))
+                ones++;
+
+        long long zeros = n - ones;
+
+        ans = (ans + (ones * zeros % MOD) * 2) % MOD;
+    }
+
+    return ans;
+}
