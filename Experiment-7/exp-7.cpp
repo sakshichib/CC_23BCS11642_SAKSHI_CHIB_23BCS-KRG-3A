3247082328feefd1314eba8dbc3a8a12f598587d
Sakshi Chib <157032642+sakshichib@users.noreply.github.com>
2026-04-03 13:31:08 +0530

    Done!

--- Experiment-7/exp-7.cpp
+++ Experiment-7/exp-7.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    
+    vector<vector<pair<int,int>>> adj(n);
+    for (auto &f : flights) {
+        adj[f[0]].push_back({f[1], f[2]});
+    }
+
+    vector<int> dist(n, INT_MAX);
+    dist[src] = 0;
+
+    queue<tuple<int,int,int>> q;
+    q.push({src, 0, 0});
+
+    while (!q.empty()) {
+        auto [node, cost, stops] = q.front();
+        q.pop();
+
+        if (stops > k) continue;
+
+        for (auto &it : adj[node]) {
+            int neighbor = it.first;
+            int price = it.second;
+
+            int newCost = cost + price;
+
+            if (newCost < dist[neighbor]) {
+                dist[neighbor] = newCost;
+                q.push({neighbor, newCost, stops + 1});
+            }
+        }
+    }
+    return (dist[dst] == INT_MAX) ? -1 : dist[dst];
+}
