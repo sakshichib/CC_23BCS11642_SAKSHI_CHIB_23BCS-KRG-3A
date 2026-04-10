c8e3c1a2aad4c70f17894e5796e58f8f6655f3a7
Sakshi Chib <157032642+sakshichib@users.noreply.github.com>
2026-04-10 10:04:56 +0530

    Done

---
+++ ClaasWork/grid_path.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countPaths(int i, int j, int m, int n, vector<vector<int>> &dp) {
+    if (i == m - 1 && j == n - 1)
+        return 1;
+
+    if (i >= m || j >= n)
+        return 0;
+
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    dp[i][j] = countPaths(i, j + 1, m, n, dp) +
+               countPaths(i + 1, j, m, n, dp);
+
+    return dp[i][j];
+}
+
+int main() {
+    int m = 2, n = 2;
+
+    vector<vector<int>> dp(m, vector<int>(n, -1));
+
+    cout << countPaths(0, 0, m, n, dp);
+}
